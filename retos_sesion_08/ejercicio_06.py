df6237f8441a754bf92230385b05dae17447ec04
andres-guzman <andres.16602@gmail.com>
2024-05-13 15:44:14 -0400

    Retos sesión 08

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,10 @@
+# De las tuplas de coordenadas
+
+# $(x1,y1) = (40, 80) $ y $ (x2,y2) = (50, 50)$
+
+# Encuentra las coordenadas del punto medio, almacena en una tupla e imprime el resultado
+
+# $(x,y)$
+
+
+# No pude :(
